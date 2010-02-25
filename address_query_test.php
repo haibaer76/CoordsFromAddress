98890c0dcc65d162c6e83fdf29885f61f6148729
Torsten Fehre <post@feson.de>
2010-02-25 12:49:02 +0100

    added some simple tests

--- address_query_test.php
+++ address_query_test.php
@@ -6,10 +6,23 @@ require_once('address_query.php');
 class AddressQuerierTest extends PHPUnit_Framework_TestCase {
 	public function testExistingAddressInHamburg() {
 		$coords = AddressQuerier::queryCoordinates("Wandsbeker Chaussee", 29, "Hamburg");
+		$this->checkCoords($coords, "Wandsbeker Chaussee 29, Hamburg, Germany", 53.565632, 10.037616);
+	}
+
+	public function testMyAddressInDresden() {
+		$coords = AddressQuerier::queryCoordinates("Bräuergasse", 11, "Dresden");
+		$this->checkCoords($coords, "Bräuergasse 11, Dresden, Germany", 51.057935, 13.720143);
+	}
+	public function testNullAddress() {
+		$coords = AddressQuerier::queryCoordinates("This Address", 8, "Should not exist");
+		$this->assertNull($coords);
+	}
+
+	private function checkCoords($coords, $name, $lat, $lon) {
 		$this->assertNotNull($coords);
-		$this->assertEquals($coords->placemark, "Wandsbeker Chaussee 29, Hamburg, Germany");
-		$this->assertEquals($coords->lon, 10.037616);
-		$this->assertEquals($coords->lat, 53.565632);
+		$this->assertEquals($coords->placemark, $name);
+		$this->assertEquals($coords->lat, $lat);
+		$this->assertEquals($coords->lon, $lon);
 	}
 }
 ?>
