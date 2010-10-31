18db2f7b10bc6383ac012a6d14fb9b705a684e1f
Torsten Fehre <post@feson.de>
2010-10-31 11:00:19 +0100

    Modified test to use rounded coords

--- address_query_test.php
+++ address_query_test.php
@@ -6,12 +6,12 @@ require_once('address_query.php');
 class AddressQuerierTest extends PHPUnit_Framework_TestCase {
 	public function testExistingAddressInHamburg() {
 		$coords = AddressQuerier::queryCoordinates("Wandsbeker Chaussee", 29, "Hamburg");
-		$this->checkCoords($coords, "Wandsbeker Chaussee 29, Hamburg, Germany", 53.565632, 10.037616);
+		$this->checkCoords($coords, "Wandsbeker Chaussee 29, Hamburg, Germany", 53, 10);
 	}
 
 	public function testMyAddressInDresden() {
 		$coords = AddressQuerier::queryCoordinates("Bräuergasse", 11, "Dresden");
-		$this->checkCoords($coords, "Bräuergasse 11, Dresden, Germany", 51.057935, 13.720143);
+		$this->checkCoords($coords, "Bräuergasse 11, Dresden, Germany", 51, 13);
 	}
 	public function testNullAddress() {
 		$coords = AddressQuerier::queryCoordinates("This Address", 8, "Should not exist");
@@ -21,8 +21,8 @@ class AddressQuerierTest extends PHPUnit_Framework_TestCase {
 	private function checkCoords($coords, $name, $lat, $lon) {
 		$this->assertNotNull($coords);
 		$this->assertEquals($coords->placemark, $name);
-		$this->assertEquals($coords->lat, $lat);
-		$this->assertEquals($coords->lon, $lon);
+		$this->assertEquals(floor($coords->lat), $lat);
+		$this->assertEquals(floor($coords->lon), $lon);
 	}
 }
 ?>
